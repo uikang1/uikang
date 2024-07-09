e77e1fa1eebeb1be56705d8c5981449cf37e98b7
uikang1 <dasuu12@gmail.com>
2024-07-09 17:19:31 +0900

    수정

--- chart.js
+++ chart.js
@@ -67,6 +67,7 @@ const radarData = {
         },
     ],
 };
+
 const radarData1 = {
     labels: ['은정', '중권', '상호', '의강'],
     datasets: [
@@ -110,130 +111,134 @@ const radarOptions = {
     },
 };
 
-function BarChart() {
-    const bar = document.getElementById('chartCanvas').getContext('2d');
-    new Chart(bar, {
-        type: 'bar',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'My First dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            scales: {
-                x: {
-                    ticks: {
-                        font: {
-                            size: 20,
-                        },
-                    },
-                },
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        font: {
-                            size: 20,
-                        },
-                    },
-                },
-            },
-        },
-    });
+function LineChart(canvasId) {
+    uikangChart('line', lineData, lineOptions, canvasId);
 }
 
-function PieChart() {
-    const pie = document.getElementById('chartCanvas').getContext('2d');
-    new Chart(pie, {
-        type: 'pie',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow'],
-            datasets: [
-                {
-                    data: [300, 50, 100],
-                    backgroundColor: ['red', 'blue', 'yellow'],
-                },
-            ],
-        },
-        options: {
-            plugins: {
-                legend: {
-                    labels: {
-                        font: {
-                            size: 20,
-                        },
-                    },
-                },
-            },
-        },
-    });
+function RadarChart(data, canvasId) {
+    uikangChart('radar', data, radarOptions, canvasId);
 }
 
-function DoughnutChart() {
-    const doughnut = document.getElementById('chartCanvas').getContext('2d');
-    new Chart(doughnut, {
-        type: 'doughnut',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow'],
-            datasets: [
-                {
-                    data: [300, 50, 100],
-                    backgroundColor: ['red', 'blue', 'yellow'],
+function BarChart(canvasId) {
+    const barData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const barOptions = {
+        scales: {
+            x: {
+                ticks: {
+                    font: {
+                        size: 20,
+                    },
                 },
-            ],
-        },
-        options: {
-            plugins: {
-                legend: {
-                    labels: {
-                        font: {
-                            size: 20,
-                        },
+            },
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        size: 20,
                     },
                 },
             },
         },
-    });
+    };
+
+    uikangChart('bar', barData, barOptions, canvasId);
 }
 
-function PolarAreaChart() {
-    const polarArea = document.getElementById('chartCanvas').getContext('2d');
-    new Chart(polarArea, {
-        type: 'polarArea',
-        data: {
-            labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
-            datasets: [
-                {
-                    data: [10, 10, 10, 10, 10],
-                    backgroundColor: ['red', 'green', 'yellow', 'grey', 'blue'],
-                },
-            ],
-        },
-        options: {
-            scales: {
-                r: {
-                    ticks: {
-                        font: {
-                            size: 20,
-                        },
-                    },
-                },
+function PieChart(canvasId) {
+    const pieData = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+            {
+                data: [300, 50, 100],
+                backgroundColor: ['red', 'blue', 'yellow'],
             },
-            plugins: {
-                legend: {
-                    labels: {
-                        font: {
-                            size: 20,
-                        },
+        ],
+    };
+
+    const pieOptions = {
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 20,
                     },
                 },
             },
         },
-    });
+    };
+
+    uikangChart('pie', pieData, pieOptions, canvasId);
+}
+
+function DoughnutChart(canvasId) {
+    const doughnutData = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+            {
+                data: [300, 50, 100],
+                backgroundColor: ['red', 'blue', 'yellow'],
+            },
+        ],
+    };
+
+    const doughnutOptions = {
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 20,
+                    },
+                },
+            },
+        },
+    };
+
+    uikangChart('doughnut', doughnutData, doughnutOptions, canvasId);
+}
+
+function PolarAreaChart(canvasId) {
+    const polarAreaData = {
+        labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+        datasets: [
+            {
+                data: [10, 10, 10, 10, 10],
+                backgroundColor: ['red', 'green', 'yellow', 'grey', 'blue'],
+            },
+        ],
+    };
+
+    const polarAreaOptions = {
+        scales: {
+            r: {
+                ticks: {
+                    font: {
+                        size: 20,
+                    },
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 20,
+                    },
+                },
+            },
+        },
+    };
+
+    uikangChart('polarArea', polarAreaData, polarAreaOptions, canvasId);
 }
